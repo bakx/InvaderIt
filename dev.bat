e7d09302ba174d6a37ab154474eb11185bdd5e65
Gideon Bakx <gideon@bakx.ca>
2019-12-10 00:25:44 -0500

    Moved the copying of Pixi files to the dist folder.

--- dev.bat
+++ dev.bat
@@ -1,2 +1,4 @@
+xcopy "./node_modules\pixi.js\dist\pixi.min.js" dist
+xcopy "./node_modules\pixi.js\dist\pixi.min.js.map" dist
 npx webpack --watch --mode="development"
-# npm run-script dev
+# npm run-script devdev
